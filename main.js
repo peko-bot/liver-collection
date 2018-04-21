322cc957facc7fa7eb92d759ed003aed7ec84b0b
zy410419243 <410419243@qq.com>
2018-04-21 20:54:59 +0800

    update
    解决跨域问题

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-04-21 16:11:50 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-04-21 18:20:51
+ * @Last Modified time: 2018-04-21 20:52:17
  */
 
 // 接收popup发来的消息
@@ -43,11 +43,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 function load_item_datas(url, callback) {
     fetch(url, {
-        // method: 'GET',
         credentials: 'include', // 强制加入凭据头
-        // headers: new Headers({
-        //     'Accept': 'application/json' // 通过头指定，获取的数据类型是JSON
-        // })
     }).then(result => {
         return result.json();
     }).then(result => {
@@ -56,17 +52,12 @@ function load_item_datas(url, callback) {
 }
 
 function upload_item_datas(data, callback) {
-    var param = {
-        user_id: '6964955',
-        data: JSON.stringify(data),
-    };
     fetch('http://localhost:8023/Memo/gbf/i_item.do', {
         method: 'POST',
-        // mode: 'cors',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
-        body: JSON.stringify(param),
+        body: 'user_id=6964955&data=' + JSON.stringify(data),
     }).then(result => {
         return result.text();
     }).then(result => {
