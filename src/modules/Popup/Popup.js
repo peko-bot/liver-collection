4e635ca8add2fd3207281ef096ad3eed26d5ffd7
zy410419243 <zy410419243@gmail.com>
2018-05-20 17:53:46 +0800

    update
    1、增加面板按钮
    2、修改打包方式

--- src/modules/Popup/Popup.js
+++ src/modules/Popup/Popup.js
@@ -2,9 +2,11 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-05-20 14:46:14 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-05-20 15:19:20
+ * @Last Modified time: 2018-05-20 17:42:45
  */
 import React, { Component } from 'react'
+
+import { Button } from 'antd'
 
 import './css/Popup.css'
 
@@ -12,17 +14,70 @@ export default class Popup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            btn_loading: false,
         }
     }
 
     componentDidMount = () => {
+        // 获得当前选中标签
+        // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        //     chrome.tabs.sendMessage(tabs[0].id, { message: 'test' }, function(response) { // popup => contentscript
+        //         console.log(response)
+        //     });
+        // });
+    }
+
+    handle_upload = () => {
+        this.setState({ btn_loading: true });
+        
+        this.load_item_datas('http://game.granbluefantasy.jp/item/article_list_by_filter_mode', result => {
+            this.load_item_datas('http://game.granbluefantasy.jp/item/recovery_and_evolution_list_by_filter_mode', recovery => {
+                recovery = this.steam_roller(recovery);
     
+                result = [...result, ...recovery];
+    
+                this.upload_item_datas(result, result => {
+                    this.setState({ btn_loading: false });
+                });
+            });
+        });
+    }
+
+    // 数组扁平化
+    steam_roller = arr => {
+        let newArr = [];
+
+        for(let item of arr) {
+            Array.isArray(item) ? newArr.push.apply(newArr, this.steam_roller(item)) : newArr.push(item);
+        }
+
+        return newArr;
+    }
+
+    load_item_datas = (url, callback) => {
+        fetch(url, {
+            credentials: 'include', // 加入cookie
+        }).then(result => result.json())
+        .then(result => callback(result));
+    }
+
+    upload_item_datas = (data, callback) => {
+        fetch('http://localhost:8023/Memo/gbf/i_item.do', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            body: 'user_id=6964955&data=' + JSON.stringify(data),
+        }).then(result => result.text()).then(result => callback(result));
     }
 
     render = () => {
-        return(
-            <div className='Popup'>Popup1</div>
+        const { btn_loading } = this.state;
+
+        return (
+            <div className='Popup'>
+                <Button type='primary' loading={ btn_loading } onClick={ this.handle_upload } style={{ width: '90%' }}>上传素材</Button>
+            </div>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-05-20 13:48:08 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-05-20 15:16:41
+ * @Last Modified time: 2018-05-20 17:11:20
  */
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,6 +10,7 @@ const fs = require('fs');
 const WebpackOnBuildPlugin = require('on-build-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const buildPath = './dist/';
 
@@ -19,56 +20,76 @@ module.exports = {
   },
   devtool: 'source-map',
   entry: {
-    Modules: './src/main.js'
+    Trunk: './src/main.js'
   },
   output: {
     path: __dirname + '/dist',
-    filename: 'Popup.[chunkHash:8].js'
+    filename: '[name].[chunkHash:8].js'
   },
   plugins: [
     new HtmlWebpackPlugin({ // 生成html
       template: './src/index.html'
     }),
     new WebpackOnBuildPlugin(stats => { // 删除dist下原有文件
-      const newlyCreatedAssets = stats.compilation.assets;
+        const newlyCreatedAssets = stats.compilation.assets;
 
-      fs.readdir(path.resolve(buildPath), (err, files) => {
-        files && files.forEach(file => {
-          if (!newlyCreatedAssets[file]) {
-            fs.unlink(path.resolve(buildPath + file), () => {});
-          }
-        });
-      })
+        fs.readdir(path.resolve(buildPath), (err, files) => {
+            files && files.forEach(file => {
+                if (!newlyCreatedAssets[file]) {
+                    fs.unlink(path.resolve(buildPath + file), () => {});
+                }
+            });
+        })
     }),
     new CopyWebpackPlugin([
-      {
-        from: __dirname + '/src/assets',
-        to: __dirname + '/dist/assets'
-      }
-    ])
+        {
+            from: __dirname + '/src/assets',
+            to: __dirname + '/dist/assets'
+        },
+        {
+            from: __dirname + '/manifest.json',
+            to: __dirname + '/dist'
+        },
+        {
+            from: __dirname + '/main.js',
+            to: __dirname + '/dist'
+        }
+    ]),
+    new MiniCssExtractPlugin({
+        filename: "[name].[chunkHash:8].css",
+        chunkFilename: "[id].[chunkHash:8].css"
+    })
   ],
   module: {
     rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015', 'stage-0', 'react']
-          }
-        }
-      }, {
-        test: /\.(png|jpg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
+        {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                // options: {
+                //     presets: ['es2015', 'stage-0', 'react']
+                // }
             }
-          }
-        ]
-      }
+        }, 
+        {
+            test: /\.(png|jpg)$/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+            ]
+        }, 
+        {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+        }
     ]
   }
 };
