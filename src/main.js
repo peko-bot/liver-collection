8182fb63202bf69817fc8db5d403eb6ca747345b
zy410419243 <zy410419243@gmail.com>
2018-05-28 17:20:28 +0800

    update
    增加react-router

--- src/main.js
+++ src/main.js
@@ -2,25 +2,37 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-05-28 15:20:13 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-05-28 15:29:50
+ * @Last Modified time: 2018-05-28 17:18:58
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Route, NavLink, HashRouter } from "react-router-dom"
+import { Route, NavLink, HashRouter } from 'react-router-dom'
 
 import RedBox from 'redbox-react'
 
-import TEST from './modules/Popup/Popup'
+import Popup from './modules/Popup/Popup'
+import Charts from './modules/Charts/Charts'
 
 const MOUNT_NODE = document.getElementById('root');
 
 try {
-    ReactDOM.render((
-        <BrowserRouter>
-            <TEST />
-        </BrowserRouter>
-    ), MOUNT_NODE);
+    ReactDOM.render(
+        <HashRouter>
+            <div>
+                <ul className='header'>
+                    <li>
+                        <NavLink to='/main'>main</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/charts'>charts</NavLink>
+                    </li>
+                </ul>
+                <Route path='/main' component={ Popup } />
+                <Route path='/charts' component={ Charts } />
+            </div>
+        </HashRouter>
+    , MOUNT_NODE);
 } catch (e) {
     ReactDOM.render(<RedBox error={e} />, MOUNT_NODE);
 }
---
+++ src/modules/Charts/Charts.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import './css/Charts.css'
+
+export default class Charts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+    
+    }
+
+    render = () => {
+        return(
+            <div className='Charts'>Charts</div>
+        )
+    }
+}
