40e74b6fe2f5c34be7f7f971404cbfe0c97ba1cc
zy410419243 <zy410419243@gmail.com>
2018-06-07 22:45:34 +0800

    update

--- main.js
+++ main.js
@@ -2,25 +2,29 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-05-30 21:58:12 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-06-07 21:49:43
+ * @Last Modified time: 2018-06-07 22:45:22
  */
+let observer = null;
 chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
     let tasks = [];
     const { message } = response;
 
     switch(message) {
         case 'init_room_listener':
-            const nodes = document.getElementsByClassName('txt-room-comment');
+            const nodes = document.getElementsByClassName('prt-wanted-list')[0];
 
-            nodes.addListener('', e => {
+            observer = new MutationObserver(mutations => {
                 
             });
 
-            for(let node of nodes) {
-                const { innerText = '' } = node;
+            observer.observe(nodes, {
+                attributes: true,
+                childList: true,
+                characterData: true
+            });
 
-                tasks.push(node);
-            }
+            // observer.disconnect();
+            // chrome.runtime.connect
         break;
 
         default:
