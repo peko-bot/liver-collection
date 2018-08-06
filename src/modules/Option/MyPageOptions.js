288e53b0097f0cd71107596b64357cfd1e8cf994
zy410419243 <zy410419243@gmail.com>
2018-08-06 22:08:45 +0800

    修改舔妻选项说明

--- src/modules/Option/MyPageOptions.js
+++ src/modules/Option/MyPageOptions.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-08-02 12:00:57
  * @Last Modified by: zy9
- * @Last Modified time: 2018-08-03 15:34:53
+ * @Last Modified time: 2018-08-06 22:08:24
  */
 import React, { Component } from 'react';
 
@@ -64,7 +64,7 @@ export default class MyPageOptions extends Component {
     	// disabled={ !STORE.get('isShowYourWife') }
     	return (
     		<div className='MyPageOptions' style={{ marginLeft: '1%' }}>
-    			<Tooltip title='开启该选项时，当你停在主页一段时间后，无关项将会被隐藏，只留立绘和背景。鼠标移入后会显示所有'>
+    			<Tooltip title='（暂时只有开关有效）开启该选项时，当你停在主页一段时间后，无关项将会被隐藏，只留立绘和背景。鼠标移入后会显示所有'>
     				<span style={{ float: 'left', color: '#666' }}>隐藏主页无关项</span>
     			</Tooltip>
     			<Switch onChange={ this.handleHideSwitch } defaultChecked={ STORE.get('isShowYourWife') } style={{ float: 'right', marginRight: '85%' }} />
