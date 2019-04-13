8b636f730744ace041130222d0a574fb8e884b2c
zy410419243 <zy410419243@gmail.com>
2019-04-13 20:20:55 +0800

    fix: clean-webpack-plugin breaking changes of configurations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,73 +1,58 @@
-const webpack = require("webpack");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const TohoLogPlugin = require("toho-log-plugin");
-const { commonModule, commonPlugin } = require("./webpack.common");
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TohoLogPlugin = require('toho-log-plugin');
+const { commonModule, commonPlugin } = require('./webpack.common');
 
-const dev = !!process.argv.includes("development");
+const dev = !!process.argv.includes('development');
 
 let plugins = commonPlugin;
 
 plugins.push(
   new CopyWebpackPlugin([
     {
-      from: __dirname + "/src/assets",
-      to: __dirname + "/dist/assets"
+      from: __dirname + '/src/assets',
+      to: __dirname + '/dist/assets',
     },
     {
-      from: __dirname + "/manifest.json",
-      to: __dirname + "/dist",
-      force: true
+      from: __dirname + '/manifest.json',
+      to: __dirname + '/dist',
+      force: true,
     },
     {
-      from: __dirname + "/contentScript/css",
-      to: __dirname + "/dist/assets/contentScript"
-    }
-  ])
+      from: __dirname + '/contentScript/css',
+      to: __dirname + '/dist/assets/contentScript',
+    },
+  ]),
 );
 
 plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
-
 plugins.push(new TohoLogPlugin({ dev, isPray: false, defaultWords: true }));
-
-dev &&
-  plugins.push(
-    new CleanWebpackPlugin(["dist"], {
-      exclude: ["mainifest.json"], // 如果不加这个，在rebuild时，不会再复制json到dist中
-      verbose: false
-    })
-  );
-
-!dev &&
-  plugins.push(
-    new CleanWebpackPlugin(["dist"], {
-      verbose: false
-    })
-  );
+plugins.push(new CleanWebpackPlugin({}));
 
 const options = {
-  mode: dev ? "development" : "production",
+  mode: dev ? 'development' : 'production',
   // watch: dev,
   devServer: {
-    port: 9099
+    port: 9099,
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
-  devtool: dev ? "source-map" : "",
+  devtool: dev ? 'source-map' : '',
   entry: {
-    background: __dirname + "/background",
-    contentScript: __dirname + "/contentScript",
-    popup: __dirname + "/src",
-    inject: __dirname + "/inject"
+    background: __dirname + '/background',
+    contentScript: __dirname + '/contentScript',
+    popup: __dirname + '/src',
+    inject: __dirname + '/inject',
   },
   output: {
-    path: __dirname + "/dist",
-    filename: "[name].js",
-    chunkFilename: dev ? "vendor/[name].[chunkHash:8].js" : "vendor/[name].js"
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    chunkFilename: dev ? 'vendor/[name].[chunkHash:8].js' : 'vendor/[name].js',
   },
   plugins,
-  module: commonModule
+  module: commonModule,
 };
 
 dev && webpack(options).watch({}, () => {});
